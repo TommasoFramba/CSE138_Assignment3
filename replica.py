89a1b6bc340e0e03ca48ed29b466a3b8b37c2df2
Tommaso Marco Framba <frambatom@gmail.com>
2021-11-11 16:52:21 -0800

    Update replica.py

--- replica.py
+++ replica.py
@@ -1,11 +1,11 @@
 ####################
 # Course: CSE138
 # Date: Fall 2021
-# Assignment: 2
+# Assignment: 3
 # Tommaso Framba
 # Eric Yao Huang
-# This program implements a simple HTTP interface with specified
-# responses to GET and PUT and DELETE requests.
+# This program implements a distributed KVS Store
+# with proper causal delivery property
 ###################
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -17,32 +17,53 @@ import socket
 import subprocess
 from html.parser import HTMLParser
 
+class http_server:
+    def __init__(self, views, address):
+        replicaStoreHandler.view = views
+        replicaStoreHandler.sock = address
+        server = HTTPServer(('', int(address[1])),replicaStoreHandler)
+        print('Server running on address ', address)
+        server.serve_forever()
 
 # handle requests
 class replicaStoreHandler(BaseHTTPRequestHandler):
     keyValueStore = dict()
     view = []
     count = 0
+    sock = ""
 
     def setView(self, view):
         self.view = view
 
+    # handle post requests
+    def do_PUT(self):
+        parsed_path = urlparse(self.path).path.split("/")
+        if len(parsed_path) == 2 and parsed_path[1] == 'view':
+            print("My sock: ", self.sock)
+            print("My view: ", self.view)
+
+
+
+
     def do_GET(self):
         print("GET!")
 
-        if self.count != 1:
-            response = requests.get("http://10.10.0.2:8090", timeout=2.50)
-            self.count = 1
+        parsed_path = self.path
+        url = "http://" + os.environ.get('SOCKET_ADDRESS')
+        host = str(os.environ.get('SOCKET_ADDRESS')).split(":")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((host[0], int(host[1])))
+        print(result)
+
+        sock.close()
+
 
 # start and run server on port 8090
 def main():
     #Get -e vars
     address = str(os.environ.get('SOCKET_ADDRESS')).split(':')
     views = os.environ.get('VIEW')
-    server = HTTPServer((address[0], int(address[1])), replicaStoreHandler)
-    print('Server running on address ', address)
-    server.serve_forever()
-
+    server = http_server(views, address)
 
 if __name__ == '__main__':
     main()
