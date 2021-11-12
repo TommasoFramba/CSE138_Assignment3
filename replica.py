b675a9ac4f0c2277fd85d876ad59f29f81a77a89
Tommaso Marco Framba <frambatom@gmail.com>
2021-11-11 17:38:32 -0800

    Update replica.py
    Done with /view GET and DELETE

--- replica.py
+++ replica.py
@@ -17,6 +17,9 @@ import socket
 import subprocess
 from html.parser import HTMLParser
 
+
+
+#setup the server to initalize its address and view as class fields
 class http_server:
     def __init__(self, views, address):
         replicaStoreHandler.view = views
@@ -32,37 +35,80 @@ class replicaStoreHandler(BaseHTTPRequestHandler):
     count = 0
     sock = ""
 
-    def setView(self, view):
-        self.view = view
+
 
     # handle post requests
     def do_PUT(self):
         parsed_path = urlparse(self.path).path.split("/")
+
+        #Put request for view
         if len(parsed_path) == 2 and parsed_path[1] == 'view':
-            print("My sock: ", self.sock)
-            print("My view: ", self.view)
 
+            #Get Json Body '{"socket-address":<NEW-REPLICA>}'
+            content_len = int(self.headers.get('content-length'))
+            body = self.rfile.read(content_len)
+            data = json.loads(body)
 
+            #Check if address is in view
+            checkAddress = data['socket-address']
+            if checkAddress in self.view:
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                jsndict = {"result": "already present"}
+                jsnrtrn = json.dumps(jsndict)
+                self.wfile.write(jsnrtrn.encode("utf8"))
+            else:
+                print("The check address is not in view send CREATED")
 
 
     def do_GET(self):
-        print("GET!")
+        parsed_path = urlparse(self.path).path.split("/")
 
-        parsed_path = self.path
-        url = "http://" + os.environ.get('SOCKET_ADDRESS')
-        host = str(os.environ.get('SOCKET_ADDRESS')).split(":")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((host[0], int(host[1])))
-        print(result)
+        # Get request for view
+        if len(parsed_path) == 2 and parsed_path[1] == 'view':
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            jsndict = {"view": self.view}
+            jsnrtrn = json.dumps(jsndict)
+            self.wfile.write(jsnrtrn.encode("utf8"))
 
-        sock.close()
 
+    def do_DELETE(self):
+        parsed_path = urlparse(self.path).path.split("/")
 
-# start and run server on port 8090
+        # Delete request for view
+        if len(parsed_path) == 2 and parsed_path[1] == 'view':
+
+            # Get Json Body '{"socket-address":<NEW-REPLICA>}'
+            content_len = int(self.headers.get('content-length'))
+            body = self.rfile.read(content_len)
+            data = json.loads(body)
+
+            # Check if address is in view
+            checkAddress = data['socket-address']
+            if checkAddress in self.view:
+                self.view.remove(checkAddress)
+                self.send_response(200)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                jsndict = {"result": "deleted"}
+                jsnrtrn = json.dumps(jsndict)
+                self.wfile.write(jsnrtrn.encode("utf8"))
+            else:
+                self.send_response(404)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                jsndict = {"error": "View has no such replica"}
+                jsnrtrn = json.dumps(jsndict)
+                self.wfile.write(jsnrtrn.encode("utf8"))
+
+# start and run server on specified port
 def main():
     #Get -e vars
     address = str(os.environ.get('SOCKET_ADDRESS')).split(':')
-    views = os.environ.get('VIEW')
+    views = str(os.environ.get('VIEW')).split(',')
     server = http_server(views, address)
 
 if __name__ == '__main__':
