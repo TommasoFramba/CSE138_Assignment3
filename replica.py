6d3521490e922d11f5b5dbbb53b99deca82a3a7e
Tommaso Marco Framba <frambatom@gmail.com>
2021-11-11 18:37:11 -0800

    Update replica.py
    View operations of replicated key-value store complete.

--- replica.py
+++ replica.py
@@ -24,6 +24,7 @@ class http_server:
     def __init__(self, views, address):
         replicaStoreHandler.view = views
         replicaStoreHandler.sock = address
+        replicaStoreHandler.startUpBroadcast(replicaStoreHandler)
         server = HTTPServer(('', int(address[1])),replicaStoreHandler)
         print('Server running on address ', address)
         server.serve_forever()
@@ -34,8 +35,32 @@ class replicaStoreHandler(BaseHTTPRequestHandler):
     view = []
     count = 0
     sock = ""
+    
+    #When a new replica is added to the system, it broadcasts a PUT-view request
+    #so the existing replicas add the new replica to their view. 
+    def startUpBroadcast(self):
+        for i in self.view:
 
+            #Don't check our own socket address
+            if i == os.environ.get('SOCKET_ADDRESS'):
+                continue
 
+            #Check if the view is up
+            host = i.split(":")
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((host[0], int(host[1])))
+            if result == 0:
+                sock.close()
+
+                #send a put /view to each view
+                url = "http://" + i + "/view"
+                jsndict = {"socket-address": os.environ.get('SOCKET_ADDRESS')}
+                data = json.dumps(jsndict)
+                response = requests.put(url, data=data, timeout=2.50)
+                print("\nresponse is: ")
+                print(response)
+            else:
+                print(i + "is not up yet")
 
     # handle post requests
     def do_PUT(self):
@@ -48,9 +73,11 @@ class replicaStoreHandler(BaseHTTPRequestHandler):
             content_len = int(self.headers.get('content-length'))
             body = self.rfile.read(content_len)
             data = json.loads(body)
+            #print("What is data: ", data)
 
             #Check if address is in view
             checkAddress = data['socket-address']
+            #print("Check address: ", checkAddress)
             if checkAddress in self.view:
                 self.send_response(200)
                 self.send_header("Content-type", "application/json")
@@ -59,7 +86,13 @@ class replicaStoreHandler(BaseHTTPRequestHandler):
                 jsnrtrn = json.dumps(jsndict)
                 self.wfile.write(jsnrtrn.encode("utf8"))
             else:
-                print("The check address is not in view send CREATED")
+                self.view.append(checkAddress)
+                self.send_response(201)
+                self.send_header("Content-type", "application/json")
+                self.end_headers()
+                jsndict = {"result": "added"}
+                jsnrtrn = json.dumps(jsndict)
+                self.wfile.write(jsnrtrn.encode("utf8"))
 
 
     def do_GET(self):
